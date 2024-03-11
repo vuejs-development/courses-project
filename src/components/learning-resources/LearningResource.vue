9c0153f0c27cf50a066b256369a65533437bf744
exalt <exaltpawa@gmail.com>
2024-03-11 22:12:22 +0200

    deleting a resource

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,9 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <base-button mode="flat">Delete</base-button>
+        <base-button mode="flat" @click="deleteResource(id)"
+          >Delete</base-button
+        >
       </header>
 
       <p>{{ description }}</p>
@@ -16,7 +18,8 @@
 import BaseButton from '../UI/BaseButton.vue';
 export default {
   components: { BaseButton },
-  props: ['title', 'description', 'link'],
+  props: ['id', 'title', 'description', 'link'],
+  inject: ['deleteResource'],
   data() {
     return {};
   },
--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -45,6 +45,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      deleteResource: this.removeResource,
     };
   },
   computed: {
@@ -69,6 +70,12 @@ export default {
       };
       this.storedResources.unshift(newResource);
       this.selectedTab = 'stored-resources';
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(
+        (res) => res.id === resId
+      );
+      this.storedResources.splice(resIndex, 1);
     },
   },
 };
